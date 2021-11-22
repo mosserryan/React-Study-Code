8952245735a9bd77f16285e8a151a298f2cec138
mosserryan <mosser.ryan.a@gmail.com>
2021-11-21 16:33:11 -0800

    Made the changes to UserDashboard to take in the NavBar.

--- src/Components/UserDashboard.js
+++ src/Components/UserDashboard.js
@@ -1,1 +1,12 @@
-const UserDashBoard = () => {};
+import NavBar from "./NavBar";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+
+const UserDashBoard = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default UserDashBoard;
