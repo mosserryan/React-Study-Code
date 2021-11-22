2caee7752b4f43a0b32166c05b63268e892a4607
mosserryan <mosser.ryan.a@gmail.com>
2021-11-21 16:32:05 -0800

    Created and finished NavBarData, made it an array with objects for each item to map through.

---
+++ src/Components/NavBarData.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const NavBarData = [
+  {
+    title: "Home",
+    icon: "fa fa-home",
+    link: "https://www.facebook.com/MauiVisionRentals",
+  },
+  {
+    title: "Home",
+    icon: "fa fa-home",
+    link: "https://www.facebook.com/MauiVisionRentals",
+  },
+  {
+    title: "Home",
+    icon: "fa fa-home",
+    link: "https://www.facebook.com/MauiVisionRentals",
+  },
+  {
+    title: "Home",
+    icon: "fa fa-home",
+    link: "https://www.facebook.com/MauiVisionRentals",
+  },
+  {
+    title: "Home",
+    icon: "fa fa-home",
+    link: "https://www.facebook.com/MauiVisionRentals",
+  },
+  {
+    title: "Home",
+    icon: "fa fa-home",
+    link: "https://www.facebook.com/MauiVisionRentals",
+  },
+];
