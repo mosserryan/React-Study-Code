544819f6049c58c2c05279f361f838b88a13ceb1
mosserryan <mosser.ryan.a@gmail.com>
2021-11-28 17:12:43 -0800

    Changes made to Navbar, fixed icons and routing.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,24 +1,34 @@
 import { NavBarData } from "./NavBarData";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import logo from "../images/MVLogo.png";
 
 const NavBar = () => {
+  let location = useLocation();
+
   return (
     <div className="sideBar">
-      {NavBarData.map((val, key) => {
-        return (
-          <ul>
-            <li>
-              <Link
-                key={key}
-                className={`test ${val.icon}`}
-                to="https://www.facebook.com/MauiVisionRentals"
-              >
-                <div>{val.title}</div>
+      <ul className="sideBarList">
+        <div className="logo">
+          <img src={logo} width="100%" />
+        </div>
+
+        {NavBarData.map((val, key) => {
+          return (
+            <li
+              key={key}
+              className="row"
+              id={location.pathname == val.link ? "active" : ""}
+              onClick={console.log(location.pathname)}
+            >
+              <Link className="link" to={val.link}>
+                <div id="icon">{val.icon}</div>
+                <div id="title">{val.title}</div>
               </Link>
             </li>
-          </ul>
-        );
-      })}
+          );
+        })}
+      </ul>
     </div>
   );
 };
